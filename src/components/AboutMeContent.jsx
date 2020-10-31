a454d218499d3ea86c534fd159c6cc9c14aae600
theCodeFox <akkitta@live.co.uk>
2020-10-31 15:42:35 +0000

    updated home page job title

--- src/components/AboutMeContent.jsx
+++ src/components/AboutMeContent.jsx
@@ -8,20 +8,22 @@ const AboutMeContent = ({ theme }) => {
       </h3>
       <p>
         I live & work in <span className={`highlight highlight-${theme}`}> Manchester city centre.</span>
-        <br />Whilst not at work, I can usually be found in the Northern Quarter typing away on my laptop in one of the many, coffee shops scattered about the place.
+        <br />During these unusual times, I spend most of my time working remotely from my home office and my daily escape walks around the Northern Quarter. I am very work focused but make sure I mentor at Code and Stuff every Thursday between 17:30 and 20:30. We are currently on Discord and you can book your free place on Eventbrite as either a mentee or mentor. For more info:
+        <br />
+        https://codeandstuff-manchester.github.io/
       </p>
       <h3 className={`highlight highlight-${theme}`}>
         <img src={require(`../images/bee-${theme}.png`)} alt="bee" height="16" width="16" />{' '}
         Languages
       </h3>
       <p>
-        <strong>HTML<br />CSS<br />JavaScript<br />TypeScript</strong>
+        <strong>HTML<br />CSS<br />JavaScript<br />PHP<br />SQL (T-SQL, GraphQL, MySQL)<br />DAX & Power M Query<br />TypeScript</strong>
       </p>
       <h3 className={`highlight highlight-${theme}`}>
         <img src={require(`../images/cog-${theme}.png`)} alt="cog" height="16" width="16" />{' '}
         What I am doing now
       </h3>
-      <p>Graduated as a Junior Full Stack Developer (April 12th 2019) from <span className={`highlight highlight-${theme}`}>Northcoders MCR</span>. I am now working as a Junior Software Engineer at <span className={`highlight highlight-${theme}`}>Ditto AI Ltd</span></p>
+      <p>Graduated as a Junior Full Stack Developer (April 12th 2019) from <span className={`highlight highlight-${theme}`}>Northcoders MCR</span>. I am now working as a Junior BI at <span className={`highlight highlight-${theme}`}>Foreign Currency Direct</span></p>
       <h3 className={`highlight highlight-${theme}`}>
         <img src={require(`../images/cloud-${theme}.png`)} alt="cloud" height="16" width="16" />{' '}
         Favourite Quote
